cbac0486dbc14ac963e832093034317167c2ce99
Diego Lopez Correa <72930512+dlopezcorrea@users.noreply.github.com>
2022-10-07 18:48:43 -0500

    creando el primer template de html

--- pacer/base/models.py
+++ pacer/base/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from sqlalchemy import null
 # Create your models here.
 
-class Tareas(models.Model):
+class Tarea(models.Model):
     usuario= models.ForeignKey(User, on_delete=models.CASCADE, null= True, blank=True)
     titulo= models.CharField(max_length=200)
     descripcion=models.TextField(null=True, blank=True)
--- pacer/base/views.py
+++ pacer/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
 
-def taskList(request):
-    return HttpResponse('Pacer')
+from .models import Tarea
+
+class TaskList(ListView):
+    model = Tarea
