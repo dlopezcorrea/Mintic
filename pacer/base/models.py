22f4bd1749350ca549073452473db0ba91bbc0ae
Diego Lopez Correa <72930512+dlopezcorrea@users.noreply.github.com>
2022-10-07 18:30:04 -0500

    creando tablas

--- pacer/base/models.py
+++ pacer/base/models.py
@@ -1,3 +1,18 @@
+from email.policy import default
 from django.db import models
-
+from django.contrib.auth.models import User
+from sqlalchemy import null
 # Create your models here.
+
+class Tareas(models.Model):
+    usuario= models.ForeignKey(User, on_delete=models.CASCADE, null= True, blank=True)
+    titulo= models.CharField(max_length=200)
+    descripcion=models.TextField(null=True, blank=True)
+    completa= models.BooleanField(default =False)
+    creacion= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        ordering = ['completa']
