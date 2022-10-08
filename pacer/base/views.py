e9a5971084ad89f016104530bd44fd016e562057
Diego Lopez Correa <72930512+dlopezcorrea@users.noreply.github.com>
2022-10-07 23:17:27 -0500

    agregamos barra de busqueda de tareas

--- pacer/base/views.py
+++ pacer/base/views.py
@@ -46,6 +46,13 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(usuario=self.request.user)
         context['count'] = context['tasks'].filter(completa=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(titulo__startswith=search_input)
+
+        context['search_input'] = search_input
+        
         return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
