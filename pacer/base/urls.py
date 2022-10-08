5e999db5b0e57e88338cb65600bce09b3dcc8e05
Diego Lopez Correa <72930512+dlopezcorrea@users.noreply.github.com>
2022-10-07 19:10:52 -0500

    agregegando lista de objetos

--- pacer/base/urls.py
+++ pacer/base/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import TaskList
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tarea')
+    path('', TaskList.as_view(), name='task')
 ]
--- pacer/base/views.py
+++ pacer/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 
-from .models import Tarea
+from .models import Task
 
 class TaskList(ListView):
-    model = Tarea
+    model = Task
